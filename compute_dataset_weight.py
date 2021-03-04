7ea3a3fbadfb5bf20ccd4a68f9b798565b65cfad
Jesús Copado <jcr.22@hotmail.com>
2021-03-04 12:05:21 +0100

    added calculation of the median frequency weights

--- compute_dataset_weight.py
+++ compute_dataset_weight.py
@@ -7,18 +7,28 @@ import numpy as np
 parser = argparse.ArgumentParser()
 parser.add_argument('--labels-dir', default=None, metavar='DIR',
                     help='path to labels')
-parser.add_argument('--output-file', default='data/weight.txt',
-                    help='path to file where to write the weights per class')
+parser.add_argument('--output-file-inv', default='data/weight.txt',
+                    help='path to file where to write the weights per class (inverse)')
+parser.add_argument('--output-file-med', default='data/weight.txt',
+                    help='path to file where to write the weights per class (median)')
 args = parser.parse_args()
 
 
 # https://github.com/ultralytics/yolov3/issues/249#L59
-def labels_to_class_weights(labels, nc=5):
+def labels_to_class_weights_inverse(labels, nc=5):
     # Get class weights (inverse frequency) from training labels
-    weights = np.bincount(labels, minlength=nc).astype(float)  # occurences per class
+    weights = np.bincount(labels, minlength=nc).astype(float)  # occurrences per class
     weights /= weights.sum()  # normalize
     weights[weights == 0] = 1  # replace empty bins with 1
     weights = 1 / weights  # number of targets per class
+    return weights
+
+
+# https://arxiv.org/pdf/1511.00561.pdf
+def labels_to_class_weights_median(labels, nc=5):
+    class_freq = np.bincount(labels, minlength=nc).astype(float)  # occurrences per class
+    median_freq = np.median(class_freq)  # median of class frequencies
+    weights = median_freq / class_freq  # ratio of the median divided by the class frequency
     return weights
 
 
@@ -28,8 +38,10 @@ def main():
         label_path = os.path.join(args.labels_dir, filename)
         label = imageio.imread(label_path).flatten()
         labels = np.concatenate((labels, label))
-    weight = labels_to_class_weights(labels - 1)
-    np.savetxt(args.output_file, weight, delimiter='\n', fmt='%.6f')
+    weight = labels_to_class_weights_inverse(labels - 1)
+    np.savetxt(args.output_file_inv, weight, delimiter='\n', fmt='%.6f')
+    med_weight = labels_to_class_weights_median(labels - 1)
+    np.savetxt(args.output_file_med, med_weight, delimiter='\n', fmt='%.6f')
 
 
 if __name__ == '__main__':
